e5c3aeefb5033684e3deb9c9a7f11dccd3dbd2fd
joengyn7 <joe.ngyn7@gmail.com>
2021-01-15 14:14:38 -0500

    learning about promises. .then(), .catch(), .finally, Promise.all()

---
+++ js/promises-breakfast.js
@@ -0,0 +1,17 @@
+
+const order = true;
+
+const breakfastPromise = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+        if (order) {
+          resolve('Your Order is Ready.');  
+        } else {
+          reject( Error('Oh no! There was a problem with your order.') );
+        }
+    }, 3000);
+});
+
+console.log(breakfastPromise);
+breakfastPromise
+    .then( val => console.log(val) )
+    .catch( err => console.log(err) )
